ae5d9ac67473fc53394858539b33e0a9b02588f1
ameeraazli <wanazliwanameera@gmail.com>
2023-06-23 10:56:31 +0800

    initializing project

---
+++ src/test/java/com/example/blog/springdatajdbc/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog.springdatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
